8f36b1b8462bf79c4f03b2d48a9a670bfb711320
Gitesh Pareek <pareekgitesh89@gmail.com>
2024-04-20 02:59:27 +0530

    fix: about and landing paging ui fix

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,51 +1,71 @@
-import React from 'react'
-import './about.css'
+import React from "react";
+import "./about.css";
 
 export default function About(props) {
-  let c = () => { if(props.mode === 'dark') {
-    return 'white';
-  }  
-   else return 'black'; }
+  let c = () => {
+    if (props.mode === "dark") {
+      return "white";
+    } else return "black";
+  };
   return (
     <section className="Team">
-      <div className="container" style={{padding:'49px'}}>
-        <h1 style={{color:c()}}>Our Team</h1>
-        <div className="row">
-          <div className="col-md-3 profile text-center">
-            <div className="img-box">
-              <img src={require('../images/GiteshD.jpg')} className="img-fluid rounded"/>
+      <div className="container " style={{ padding: "49px" }}>
+        <h1 style={{ color: c() }}>Developers</h1>
+        <div className="center">
+          <div className="row size">
+            <div className="col profile text-center">
+              <div className="img-box">
+                <img
+                  src={require("../images/GiteshD.jpg")}
+                  className="img-fluid rounded"
+                />
                 <ul>
-                  <a href="https://twitter.com/PareeKGitesh89"  target='_blank'><li><i className="fa fa-twitter"></i></li></a>
-                  <a href="https://www.linkedin.com/in/gitesh-pareek-146a78213/"  target='_blank'><li><i className="fa fa-linkedin"></i></li></a>
+                  <a href="https://twitter.com/PareeKGitesh89" target="_blank">
+                    <li>
+                      <i className="fa fa-twitter"></i>
+                    </li>
+                  </a>
+                  <a
+                    href="https://www.linkedin.com/in/gitesh-pareek-146a78213/"
+                    target="_blank"
+                  >
+                    <li>
+                      <i className="fa fa-linkedin"></i>
+                    </li>
+                  </a>
                 </ul>
+              </div>
+              <h2>Gitesh Pareek</h2>
+              <h4>MERN Developer</h4>
             </div>
-            <h2>Gitesh Pareek</h2>
-            <h4>MERN Developer</h4>
-          </div>
-          <div className="col-md-3 profile text-center">
-            <div className="img-box">
-              <img src={require('../images/AnirudhD.jpg')} className="img-fluid rounded"/>
+            <div className="col profile text-center">
+              <div className="img-box">
+                <img
+                  src={require("../images/AnirudhD.jpg")}
+                  className="img-fluid rounded"
+                />
                 <ul>
-                  <a href="https://twitter.com/bot_Anirudh"  target='_blank'><li><i className="fa fa-twitter"></i></li></a>
-                  <a href="https://www.linkedin.com/in/anirudh-sharma-24516b215/"  target='_blank'><li><i className="fa fa-linkedin"></i></li></a>
+                  <a href="https://twitter.com/bot_Anirudh" target="_blank">
+                    <li>
+                      <i className="fa fa-twitter"></i>
+                    </li>
+                  </a>
+                  <a
+                    href="https://www.linkedin.com/in/anirudh-sharma-24516b215/"
+                    target="_blank"
+                  >
+                    <li>
+                      <i className="fa fa-linkedin"></i>
+                    </li>
+                  </a>
                 </ul>
+              </div>
+              <h2>Anirudh Sharma</h2>
+              <h4>MERN Developer</h4>
             </div>
-            <h2>Anirudh Sharma</h2>
-            <h4>MERN Developer</h4>
-          </div>
-          <div className="col-md-3 profile text-center">
-            <div className="img-box">
-              <img src={require('../images/HarshitaD.jpg')} className="img-fluid rounded"/>
-                <ul>
-                  <a href="https://twitter.com/HarshitaPal18"  target='_blank'><li><i className="fa fa-twitter"></i></li></a>
-                  <a href="https://www.linkedin.com/in/harshita-pal-1848751bb/"  target='_blank'><li><i className="fa fa-linkedin"></i></li></a>
-                </ul>
-            </div>
-            <h2>Harshita Pal</h2>
-            <h4>UI/UX Designer / ReactJs Developer</h4>
           </div>
         </div>
       </div>
     </section>
-  )
+  );
 }
